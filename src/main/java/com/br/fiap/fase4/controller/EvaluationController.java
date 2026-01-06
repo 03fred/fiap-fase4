d1b63e04458e8d7dbc0fb4c45ac7c2c61edda33a
root <frederico.d.silva@outlook.com>
2026-01-06 11:41:35 -0300

    feat event

--- src/main/java/com/br/fiap/fase4/controller/EvaluationController.java
+++ src/main/java/com/br/fiap/fase4/controller/EvaluationController.java
@@ -1,6 +1,5 @@
 package com.br.fiap.fase4.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.azure.core.util.BinaryData;
+import com.azure.messaging.eventgrid.EventGridEvent;
+import com.azure.messaging.eventgrid.EventGridPublisherClient;
 import com.br.fiap.fase4.dto.EvaluationDTO;
 import com.br.fiap.fase4.interfaces.services.EvaluationService;
 import com.br.fiap.fase4.model.Evaluation;
@@ -19,14 +21,26 @@ import com.br.fiap.fase4.model.Evaluation;
 public class EvaluationController {
 
 	private EvaluationService service;
-
-	public EvaluationController(EvaluationService service) {
+    private final EventGridPublisherClient<EventGridEvent> client;
+    
+	public EvaluationController(EvaluationService service, EventGridPublisherClient<EventGridEvent> client) {
 		this.service = service;
+		this.client = client;
 	}
 
 	@PostMapping("")
 	public ResponseEntity<Void> save(@RequestBody EvaluationDTO dto) {
 		this.service.save(dto);
+		
+		EventGridEvent event = new EventGridEvent(
+                "evaluation",
+                "EvaluationCreated",
+                 BinaryData.fromObject(dto),
+                "1.0"
+        );
+
+        client.sendEvent(event);
+        
 		return ResponseEntity.status(201).build();
 	}
 
