08870d0e62f0bf7ce19e18793bf3458d66fe497e
root <frederico.d.silva@outlook.com>
2026-01-06 11:41:46 -0300

    feat event

---
+++ src/main/java/com/br/fiap/fase4/conf/EventGridConfig.java
@@ -0,0 +1,22 @@
+package com.br.fiap.fase4.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.messaging.eventgrid.EventGridEvent;
+import com.azure.messaging.eventgrid.EventGridPublisherClient;
+import com.azure.messaging.eventgrid.EventGridPublisherClientBuilder;
+
+@Configuration
+public class EventGridConfig {
+
+    @Bean
+    public EventGridPublisherClient<EventGridEvent> eventGridClient() {
+
+        return new EventGridPublisherClientBuilder()
+                .endpoint("https://evaluation-topic.brazilsouth-1.eventgrid.azure.net/api/events")
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildEventGridEventPublisherClient();
+    }
+}
